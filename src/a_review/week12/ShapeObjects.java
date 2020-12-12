cc76e01ba996b42ab47da73290ce004a6ff575ff
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-12 15:02:38 -0500

    Week 11 Review

---
+++ src/a_review/week12/ShapeObjects.java
@@ -0,0 +1,15 @@
+package a_review.week12;
+
+public class ShapeObjects {
+
+    public static void main(String[] args) {
+        //Rectangle:  w= 10,  l= 20
+        Rectangle rectangle = new Rectangle(10, 20);
+
+        System.out.println(rectangle);
+
+
+    }
+
+
+}
