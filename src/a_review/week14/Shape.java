f4cefe4c5b53cc54342c72feedfbd07bd267209d
saimCybertek <saim@cybertekschool.com>
2020-12-26 10:34:07 -0600

    Week 14 review

---
+++ src/a_review/week14/Shape.java
@@ -0,0 +1,27 @@
+package a_review.week14;
+
+    /*
+
+
+    1. create an abstract class called Shape
+			attributes:
+				name(static), area, perimeter, hasVolume(static), volume
+			abstract methods: calculateArea(), calculatePerimeter(), calculateVolume()
+						return-types: double
+
+
+     */
+
+public abstract class Shape {
+
+    static String name;
+    double area;
+    double perimeter;
+    static boolean hasVolume;
+    double volume;
+
+    public abstract double calculateArea();
+    public abstract double calculatePerimeter();
+    public abstract double calculateVolume();
+
+}
