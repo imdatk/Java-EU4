1f829752c31a553a8b6ac9e270b2d55ffbd7f4df
saimCybertek <saim@cybertekschool.com>
2021-01-02 10:51:30 -0600

    Week 15 Review session

---
+++ src/a_review/week15/PhoneObjects.java
@@ -0,0 +1,19 @@
+package a_review.week15;
+
+public class PhoneObjects {
+    public static void main(String[] args) {
+
+        IPhone phone1 = new IPhone("11", 8.0, 1999);
+        Phone phone2 = new IPhone("8", 6.5, 1000);
+        Object phone3 = new IPhone("5", 6.5, 500);
+        AppleApp phone4 = new IPhone("7", 7.0, 1500);
+
+        phone1.faceTiming(12313313);
+        ((IPhone) phone2).faceTiming(124124121);
+        ((IPhone) phone4).faceTiming(31213241);
+
+
+
+
+    }
+}
